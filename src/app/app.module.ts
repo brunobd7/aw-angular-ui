908f061673bc7acf0b41d74f8e01504f9500a0bb
Bruno Dantas <brunoodantas7@gmail.com>
2023-03-23 18:37:54 -0300

    Revert "basic nav bar hmtl in progress"
    This reverts commit ea90f10a59f5104c11a4e432faf4b51a9658f5c1.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,11 @@ import {ButtonModule} from 'primeng/button';
 import {TableModule} from 'primeng/table'
 import { TooltipModule } from 'primeng/tooltip';
 import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
-import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LancamentosPesquisaComponent,
-    NavbarComponent
+    LancamentosPesquisaComponent
   ],
   imports: [
     BrowserModule,
