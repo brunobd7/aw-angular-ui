40f81757cdc8cb3f4391e5f03e8613123aeb31f3
Bruno Dantas <brunoodantas7@gmail.com>
2023-04-29 13:06:55 -0300

    Work on data table view of primeng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {TabViewModule} from "primeng/tabview";
 import {InputTextModule} from "primeng/inputtext";
 import {ButtonModule} from "primeng/button";
+import {TableModule} from "primeng/table";
 
 @NgModule({
   declarations: [
@@ -13,10 +12,9 @@ import {ButtonModule} from "primeng/button";
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    TabViewModule,
     InputTextModule,
-    ButtonModule
+    ButtonModule,
+    TableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
